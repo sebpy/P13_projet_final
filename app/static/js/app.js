12bdef53d26f28f72baf117ceeb6c011695567ad
zelix25 <zelix_25@hotmail.fr>
2019-07-29 20:11:57 +0200

    ajout du rafraissiment des donnees json via ajax

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,18 +1,57 @@
+// Rafraichissement des données toutes les 30sec
 $( document ).ready(function() {
-    //setInterval(function(){
-        $.ajax({
-            type:"GET",
-            url: $SCRIPT_ROOT + '/_answer',
+    $.ajax({
+        type:"GET",
+        url: $SCRIPT_ROOT + '/_answer',
         }).done(function(api_resp){
+            $('#row').empty()
             $.each( api_resp, function( key, value ) {
-              $('#nom_rig').text(value.NomRig),
-              $('#hash_rig').text(""+value.HashTotal+""),
-              $('#pw_rig').text(value.totalpw);
+                if ( value.enLigne == "1") {
+                   rig_card = "rig_card_on"
+                }
+                else {
+                   rig_card = "rig_card_off"
+                }
+                $('#row').append(
+                      '<div class="'+ rig_card +'">' +
+                      '<strong>'+ value.NomRig +'</strong><br>' +
+                      'GPUs: <strong>'+ value.nbGpu +' '+ value.typeGpu +'</strong><br>' +
+                      'Uptime: <strong>'+ value.uptime +'</strong><br>' +
+                      'Miner uptime: <strong>'+ value.mineTime +'</strong><br>' +
+                      'Hashrate: <strong>'+ value.HashTotal +' '+ value.hashUnit +'</strong><br>' +
+                      'Puissance: <strong>'+ value.totalpw +'W</strong><br>' +
+                  '</div>'
+                );
             });
-        });
-    //}, 60000); // Ici chaque minute
+        }
+   );
 });
 
-
-
-
+setInterval(function(){
+    $.ajax({
+        type:"GET",
+        url: $SCRIPT_ROOT + '/_answer',
+        }).done(function(api_resp){
+            $('#row').empty()
+            var rig_car = ""
+            $.each( api_resp, function( key, value ) {
+                if ( value.enLigne == "1") {
+                   rig_card = "rig_card_on"
+                }
+                else {
+                   rig_card = "rig_card_off"
+                }
+                $('#row').append(
+                      '<div class="'+ rig_card +'">' +
+                      '<strong>'+ value.NomRig +'</strong><br>' +
+                      'GPUs: <strong>'+ value.nbGpu +' '+ value.typeGpu +'</strong><br>' +
+                      'Uptime: <strong>'+ value.uptime +'</strong><br>' +
+                      'Miner uptime: <strong>'+ value.mineTime +'</strong><br>' +
+                      'Hashrate: <strong>'+ value.HashTotal +' '+ value.hashUnit +'</strong><br>' +
+                      'Puissance: <strong>'+ value.totalpw +'W</strong><br>' +
+                  '</div>'
+                );
+            });
+        }
+   );
+}, 30000); //30 secondes
