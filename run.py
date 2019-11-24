c2dd619b365fed17ce49248d6d8ad19313bddd2a
zelix25 <zelix_25@hotmail.fr>
2019-11-24 15:16:20 +0100

    test auto update whiout apschedule

--- run.py
+++ run.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 from app import app
-
+from cron import timed_job
+import time
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
 
+    while True:
+        timed_job()
+        time.sleep(120)
