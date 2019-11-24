f9b6a83c8cee0fdcf310909cfc6528a44e230a3c
zelix25 <zelix_25@hotmail.fr>
2019-11-24 20:47:25 +0100

    change config.py

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: gunicorn run:app
-init: FLASK_APP=run.py flask init_db
+clock: python cron.py
--- config.py
+++ config.py
@@ -1,11 +1,6 @@
 import os
 SECRET_KEY = '}:Nq&)"F$9gEYmyVoY`MaB}'
 
-if os.environ.get('DATABASE_URL') is None:
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'emoslive.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-else:
-    # Postgresql Heroku
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'emoslive.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
