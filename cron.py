d408fd1ca0bf36ce8a09cad8c40486438337708b
zelix25 <zelix_25@hotmail.fr>
2019-11-24 20:08:51 +0100

    remove apschedule

--- cron.py
+++ cron.py
@@ -1,23 +1,26 @@
 #!/usr/bin/env python
 
 from app.emos import Statistics
+import time
 
-from apscheduler.schedulers.blocking import BlockingScheduler
-sched = BlockingScheduler()
+#from apscheduler.schedulers.blocking import BlockingScheduler
+#sched = BlockingScheduler()
 
 
 def cron():
-    """ Execute all commands for get stats without browser web open"""
-    api_answer = Statistics()
-    cfg_block = api_answer.read_full_conf()
-    api_resp = api_answer.get_status(cfg_block)
-    api_answer.events_save(api_resp)
-    api_answer.list_rigs(api_resp)
-    api_answer.update_stats_rig(api_resp)
+    #    """ Execute all commands for get stats without browser web open"""
+    while True:
+        api_answer = Statistics()
+        cfg_block = api_answer.read_full_conf()
+        api_resp = api_answer.get_status(cfg_block)
+        api_answer.events_save(api_resp)
+        api_answer.list_rigs(api_resp)
+        api_answer.update_stats_rig(api_resp)
+        api_answer.delete_old_stats()
+        print('ok')
+        time.sleep(20)
 
-    api_answer.delete_old_stats()
-    print('ok')
 
 
-sched.add_job(cron, "interval", seconds=10)
-sched.start()
+#sched.add_job(cron, "interval", seconds=10)
+#sched.start()
--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 from app import app
-
+from cron import cron
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
+    cron()
 
