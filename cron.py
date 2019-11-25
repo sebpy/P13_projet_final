6a1d2f6ca3e59ab543b5a3634d8d9d7c5c807256
zelix25 <zelix_25@hotmail.fr>
2019-11-25 12:50:48 +0100

    test cron

--- cron.py
+++ cron.py
@@ -11,7 +11,7 @@ def cron():
     api_answer = Statistics()
     cfg_block = api_answer.read_full_conf()
     api_resp = api_answer.get_status(cfg_block)
-    api_answer.events_save(api_resp)
+    #api_answer.events_save(api_resp)
     api_answer.list_rigs(api_resp)
     #api_answer.update_stats_rig(api_resp)
     #api_answer.delete_old_stats()
