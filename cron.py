0eae368c5644952964670b6ec69274e81bf95dd3
zelix25 <zelix_25@hotmail.fr>
2019-11-25 23:05:20 +0100

    test cron

--- cron.py
+++ cron.py
@@ -25,6 +25,7 @@ if __name__ == '__main__':
         while True:
             cron()
             time.sleep(60)
+            print('wait 60 sec')
     except (KeyboardInterrupt, SystemExit):
         print('Error!')
 
